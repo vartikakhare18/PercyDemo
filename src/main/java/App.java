5c65a6053f8c15d46bc3159198b4e198fbc1d3bc
Vartika Khare <vkhare@netspend.com>
2022-08-25 19:08:53 +0530

    initial commit

---
+++ src/main/java/App.java
@@ -0,0 +1,86 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.Executor;
+
+import com.sun.net.httpserver.HttpContext;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpServer;
+
+/**
+ * Entrypoint to the Percy Java example app: an HTTP server that serves the
+ * TodoMVC JavaScript app.
+ */
+public class App {
+    // Server port.
+    private static final Integer PORT = 8000;
+    // This file path is relative to that classpath root.
+    private static final String INDEX_PATH = "index.html";
+    // Recognized Mime type map (extension -> mimetype)
+    private static final Map<String, String> MIME_MAP = new HashMap<String, String>();
+    static {
+        MIME_MAP.put("html", "text/html");
+        MIME_MAP.put("js", "application/javascript");
+        MIME_MAP.put("css", "text/css");
+    }
+
+    public static void main(String[] args) throws IOException {
+        // Using a null executor will cause the server to run on the current thread, thus
+        // blocking execution until the server exits or this process is terminated.
+        startServer(null);
+    }
+
+    public static HttpServer startServer(Executor executor) throws IOException {
+        HttpServer server = HttpServer.create(new InetSocketAddress(PORT), 0);
+        HttpContext context = server.createContext("/");
+        context.setHandler(App::handleRequest);
+        System.out.println(String.format("Starting server, listening on port %d", PORT));
+        System.out.println("CTRL-C to exit");
+        server.setExecutor(executor);
+        server.start();
+        return server;
+    }
+
+    private static void handleRequest(HttpExchange exchange) throws IOException {
+        String requestedPath = exchange.getRequestURI().getPath();
+        System.out.println("Serving path: " + requestedPath);
+        if (requestedPath.equals("/")) {
+            serveStaticFile(exchange, INDEX_PATH);
+        } else {
+            if (requestedPath.startsWith("/")) {
+                requestedPath = requestedPath.substring(1);
+            }
+            serveStaticFile(exchange, requestedPath);
+        }
+    }
+
+    private static void serveStaticFile(HttpExchange exchange, String resourcePath) throws IOException {
+        byte[] response;
+        int responseCode;
+        InputStream in = App.class.getClassLoader().getResourceAsStream(resourcePath);
+        if (in == null) {
+            response = "404 - File Not Found".getBytes();
+            responseCode = 404;
+        } else {
+            InputStream stream = App.class.getClassLoader().getResourceAsStream(resourcePath);
+            response = new byte[stream.available()];
+            stream.read(response);
+            responseCode = 200;
+        }
+
+        exchange.getResponseHeaders().add("Content-Type", getMimeType(resourcePath));
+        exchange.sendResponseHeaders(responseCode, response.length);
+        OutputStream os = exchange.getResponseBody();
+        os.write(response);
+        os.close();
+    }
+
+    private static String getMimeType(String resourcePath) {
+        int lastDotIndex = resourcePath.lastIndexOf('.');
+        String extension = lastDotIndex > 0 ? resourcePath.substring(lastDotIndex + 1) : "";
+        return MIME_MAP.getOrDefault(extension, "text/plain");
+    }
+}
---
+++ src/test/java/AppTest.java
@@ -0,0 +1,104 @@
+import com.sun.net.httpserver.HttpServer;
+import io.percy.selenium.Percy;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+/**
+ * Unit test for example App.
+ */
+public class AppTest {
+    private static final String TEST_URL = "http://localhost:8000";
+    private static ExecutorService serverExecutor;
+    private static HttpServer server;
+    private static WebDriver driver;
+    private static Percy percy;
+
+    @BeforeEach
+    public void startAppAndOpenBrowser() throws IOException {
+        // Disable browser logs from being logged to stdout
+        System.setProperty(FirefoxDriver.SystemProperty.BROWSER_LOGFILE,"/dev/null");
+        // Create a threadpool with 1 thread and run our server on it.
+        serverExecutor = Executors.newFixedThreadPool(1);
+        server = App.startServer(serverExecutor);
+        FirefoxOptions options = new FirefoxOptions();
+        options.setHeadless(true);
+        driver = new FirefoxDriver(options);
+        percy = new Percy(driver);
+    }
+
+    @AfterEach
+    public void closeBrowser() {
+        // Close our test browser.
+        driver.quit();
+        // Shutdown our server and make sure the threadpool also terminates.
+        server.stop(1);
+        serverExecutor.shutdownNow();
+    }
+
+    @Test
+    public void loadsHomePage() {
+        driver.get(TEST_URL);
+        WebElement element = driver.findElement(By.className("todoapp"));
+        assertNotNull(element);
+
+        // Take a Percy snapshot.
+        percy.snapshot("Home Page");
+    }
+
+    @Test
+    public void acceptsANewTodo() {
+        driver.get(TEST_URL);
+
+        // We start with zero todos.
+        List<WebElement> todoEls = driver.findElements(By.cssSelector(".todo-list li"));
+        assertEquals(0, todoEls.size());
+
+        // Add a todo in the browser.
+        WebElement newTodoEl = driver.findElement(By.className("new-todo"));
+        newTodoEl.sendKeys("A new fancy todo!");
+        newTodoEl.sendKeys(Keys.RETURN);
+
+        // Now we should have 1 todo.
+        todoEls = driver.findElements(By.cssSelector(".todo-list li"));
+        assertEquals(1, todoEls.size());
+
+        // Take a Percy snapshot specifying browser widths.
+        percy.snapshot("One todo", Arrays.asList(768, 992, 1200));
+    }
+
+    @Test
+    public void letsYouCheckOffATodo() {
+        driver.get(TEST_URL);
+
+        WebElement newTodoEl = driver.findElement(By.className("new-todo"));
+        newTodoEl.sendKeys("A new todo to check off");
+        newTodoEl.sendKeys(Keys.RETURN);
+
+        WebElement todoCountEl = driver.findElement(By.className("todo-count"));
+        assertEquals("1 item left", todoCountEl.getText());
+
+        driver.findElement(By.cssSelector("input.toggle")).click();
+
+        todoCountEl = driver.findElement(By.className("todo-count"));
+        assertEquals("0 items left", todoCountEl.getText());
+
+        // Take a Percy snapshot specifying a minimum height.
+        percy.snapshot("Checked off todo", null, 2000);
+    }
+}

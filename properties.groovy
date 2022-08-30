d81bb29af4d47e59faa07780db7b9e8b54675277
Vartika Khare <vkhare@netspend.com>
2022-08-30 16:15:27 +0530

    Code changed

---
+++ properties.groovy
@@ -0,0 +1,3 @@
+buildtoolsVersion = '2018.3.0.0-SNAPSHOT'
+springBootVersion='1.5.9.RELEASE'
+artifactoryURL='http://artifactory.test.aus.netspend.net/artifactory'
---
+++ src/main/groovy/com/netspend/TestRun.groovy
@@ -0,0 +1,15 @@
+import io.percy.selenium.Percy
+import org.openqa.selenium.WebDriver
+import org.openqa.selenium.chrome.ChromeDriver
+
+class TestRun {
+    static void main(String[] args){
+        System.setProperty("webdriver.chrome.driver","C:\\dev\\Project\\ns_tester\\src\\main\\resources\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver()
+        Percy percy;
+        percy = new Percy(driver);
+        driver.get("http://localhost:8080")
+        percy.snapshot("Home Page");
+        driver.close();
+    }
+}
